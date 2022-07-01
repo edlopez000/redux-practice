076e3f3a287554129f8f1876891e70bed663548b
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-06-30 23:57:59 -0400

    One more example of how higher order functions appear in Redux

--- functionalExamples/higherOrder.ts
+++ functionalExamples/higherOrder.ts
@@ -2,6 +2,10 @@
 // or return functions
 // Examples of higher order functions are: map(), reduce(), filter()
 
+// This explains the funky looking connect() function used at the end of some older Redux code
+// connect() is actually a Higher Order Component: https://reactjs.org/docs/higher-order-components.html
+// Note: connect() is being pushed aside for the Hooks based API
+
 const weekTemps = [90, 95, 89, 91, 85];
 
 // Filter takes in a callback function as an argument
